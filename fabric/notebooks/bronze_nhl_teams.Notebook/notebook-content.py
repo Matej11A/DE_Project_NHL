09a1b9b72eb35924db00c1d44f877b9b4791f8bd
Matej Adamec <109510694+Matej11A@users.noreply.github.com>
2026-07-14 11:21:48 +0100

    dev1

--- fabric/notebooks/bronze_nhl_teams.Notebook/notebook-content.py
+++ fabric/notebooks/bronze_nhl_teams.Notebook/notebook-content.py
@@ -6,18 +6,61 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "7fa7e213-3f48-43a9-9854-240338af99f8",
+# META       "default_lakehouse_name": "lh_Main",
+# META       "default_lakehouse_workspace_id": "4dde6d65-494a-490e-895a-613e38da7758",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "7fa7e213-3f48-43a9-9854-240338af99f8"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # MARKDOWN ********************
 
-# # This is a first test of a git integration
+# # notebook to pull raw data for nhl Teams at bronze layer
 
 # CELL ********************
 
-# test code
+%pip install nhl-api-py
 
-df = spark.read.format("delta").load(f"{bronze_path}")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from nhlpy import NHLClient
+from pyspark.sql import functions as F
+import json
+
+client = NHLClient()
+data = client.teams.teams()
+
+df_bronze = spark.createDataFrame(data)
+
+df_bronze = df_bronze.withColumn("_ingested_at", F.current_timestamp())
+
+df_bronze.write.format("delta").mode("overwrite").saveAsTable("lh_Main.bronze.dim_nhl_teams")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+spark.sql("SHOW SCHEMAS IN lh_Main").show()
 
 # METADATA ********************
 
